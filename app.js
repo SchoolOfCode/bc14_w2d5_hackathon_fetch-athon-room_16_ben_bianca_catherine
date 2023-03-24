1b36e51a4bd7faaefd750a384a5ef552c59e0de4
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-03-24 15:58:00 +0000

    added CSS styles

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ retrieve the data from the api and store in a variable ✅
 *** Task 3 - Game Area
 
 - Ask each question ✅ (one at a time and wait for the user to answer)
-- When an answer is received, check if it is correct and store the result
-- Ask next question
+- When an answer is received, check if it is correct (and store the result) ✅
+- ((Ask next question))
 
 *** Task 4 - Scoring
 
@@ -61,12 +61,14 @@ function welcomeAlert() {
   let welcome = alert('Welcome to the Quiz Generator');
   return welcome;
 }
+
 welcomeAlert();
 // Instructions button
 let instructionsButton = document.getElementById('instructions');
+
 // Display instructions on how to play the game
 function instructions() {
-  let instructions = window.alert('Instructions: \n 1. Choose a category \n 2. Choose a difficulty level \n 3. Choose the number of questions \n 4. Click the Generate Quiz button \n 5. Answer the questions \n 6. Click the Submit button \n 7. See your score');
+  let instructions = window.alert('Instructions: \n 1. Choose a category \n 2. Choose a difficulty level \n 3. Click the Generate Quiz button \n 5. Test your knowledge! \n 6. Refresh the page to play again');
   //console.log(instructions)
   return instructions;
 }
@@ -78,19 +80,17 @@ let playerName = prompt('Please enter your name');
 document.getElementById('playerName').textContent = playerName;
 
 
-
 // Task 2 - Quiz Generator
-
-//const categoryChoice = document.getElementById('category-select');
-//const categorySelection = categoryChoice.value;
 
 // create a function to save the players quiz choices and stores them as variables
 
 let category = document.getElementById('category-select').value;
 let difficulty = document.getElementById('difficulty-select').value;
 
+// Create an empty array for questions
 let quizQuestions = [];
 
+// Takes in choices and returns the information from the API
 function getQuizOptions() {
   const category = document.querySelector("#category-select").value;
   const difficulty = document.querySelector("#difficulty-select").value;
@@ -107,19 +107,19 @@ function getQuizOptions() {
         };
       });
       //console.log("Quiz questions:", quizQuestions[0].question);
-      // Do something with the quizQuestions array, like display the questions to the user
+      // call the function 
       displayQuizQuestions(quizQuestions);
-      //console.log(data)// Do something with the API response data, like display the quiz questions
     })
-    // call a function 
+    
     .catch(error => {
       console.error("Error fetching quiz questions:", error);
     });
-
-  //console.log("Selected Category:", category);
-  //console.log("Selected Difficulty:", difficulty);
+  // console.log(quizQuestions);
+  // console.log("Selected Category:", category);
+  // console.log("Selected Difficulty:", difficulty);
 }
     
+// Displays questions
 function displayQuizQuestions(quizQuestions) {
   // Get a reference to the quiz container element
   const quizContainer = document.querySelector("#quiz-container");
@@ -134,13 +134,15 @@ function displayQuizQuestions(quizQuestions) {
 
     const trueButton = document.createElement("button");
     trueButton.textContent = "True";
+    trueButton.style.marginLeft = "10px";
     questionElement.appendChild(trueButton);
 
     const falseButton = document.createElement("button");
     falseButton.textContent = "False";
+    falseButton.style.marginLeft = "10px";
     questionElement.appendChild(falseButton);
 
-    // Add event listeners to the true/false buttons
+    // Add event listeners to the buttons to check answers
     trueButton.addEventListener("click", () => {
       checkAnswer("True", question.correctAnswer);
   });
@@ -155,103 +157,20 @@ function displayQuizQuestions(quizQuestions) {
           // User's answer is correct
           const correctAnswerElement = document.createElement("div");
           correctAnswerElement.textContent = "Correct!";
+          correctAnswerElement.style.fontSize = "18px";
           questionElement.appendChild(correctAnswerElement);
          // console.log("Correct!");
       } else {
           // User's answer is incorrect
           const incorrectAnswerElement = document.createElement("div");
           incorrectAnswerElement.textContent = "Incorrect!";
+          incorrectAnswerElement.style.fontSize = "18px";
           questionElement.appendChild(incorrectAnswerElement);
          // console.log("Incorrect!");
       }
   }
   });
-/* function displayQuizQuestions(quizQuestions) {
-  // Get a reference to the quiz container element
-  const quizContainer = document.querySelector("#quiz-container");
-
-  quizContainer.innerHTML = "";
-
-  const userAnswer = {};
-
-  // Loop through each quiz question and create a new element to display it
-  quizQuestions.forEach((question, index) => {
-    const questionElement = document.createElement("div");
-    questionElement.textContent = `${index + 1}. ${question.question}`;
-    quizContainer.appendChild(questionElement);
-
-    const trueButton = document.createElement("button");
-    trueButton.textContent = "True";
-    questionElement.appendChild(trueButton);
-
-    const falseButton = document.createElement("button");
-    falseButton.textContent = "False";
-    questionElement.appendChild(falseButton);
-/*
-    // Add event listeners to the true/false buttons
-    trueButton.addEventListener("click", () => {
-      selectAnswer("True", question.correctAnswer);
-  });
-
-    falseButton.addEventListener("click", () => {
-      selectAnswer("False", question.correctAnswer);
-  });
-
-  // Function to select the user's answer and store it in the userAnswers object
-  function selectAnswer(selectedButton, otherButton, answer) {
-    /* Add CSS class to selected button to style as selected
-    selectedButton.classList.add("selected");
-
-    // Remove CSS class from other button to style as unselected
-    otherButton.classList.remove("selected");
-
-    // Store the user's answer for this question in the userAnswers object
-    userAnswers[index] = answer; 
-}
-);
 }
 
-// Function to calculate the user's score
-function calculateScore() {
-  let score = 0;
-
-  quizQuestions.forEach((question, index) => {
-      if (userAnswers[index] === question.correctAnswer) {
-          // User's answer is correct
-          score++;
-      }
-  });
-
-  return score;
-}
-*/
-// console.log(calculateScore());
-
-
-
-
-}
-
-// Example usage:
-// Call calculateScore() after the user has answered all questions to get the user's score
-console.log(`Your score is ${calculateScore()} out of ${quizQuestions.length}.`);
-
-//let quizOptions2 = getQuizOptions();
-//console.log(quizOptions2);
-//console.log(getQuizOptions());
-
-const generateQuizButton = document.getElementById('generate-quiz');
-
-/*async function getQuote() {
-
-  let category = 12;
-
-  const response = await fetch(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=easy&type=boolean`);
-  const data = await response.json();
-
-  
-  let question = data.results[0].question;
-  //console.log(question);
-  // return data.results[0].question;
-}*/
+// const generateQuizButton = document.getElementById('generate-quiz');
 
